8102132f465410c4825f9ce67af70074cc3ab7db
James Meyer <JM@TrueBits.net>
2014-02-02 23:26:35 -0800

    Added occupancy manager

---
+++ HotelApp/OccupancyManager.Tests/OccupancyManagerUnitTests.cs
@@ -0,0 +1,102 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Media.Media3D;
+using HotelCorp.HotelApp.Services.Access;
+using JamesMeyer.IocContainer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceModelEx.Hosting;
+
+#endregion
+
+namespace HotelCorp.HotelApp.Services.Managers.Tests {
+    
+    [TestClass]
+    public class OccupancyManagerUnitTests {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context) {
+            using (var x = InProcFactory.CreateInstance<RoomAccess, IRoomAccess>() ) {}
+            using (var x = InProcFactory.CreateInstance<RoomAccess, IRoomAccess>()) { }
+        }
+
+        [TestMethod]
+        public void InstantiateOccupancyManager_Wcf() {
+            // Arrange
+
+            // Act
+            var manager = new OccupancyManager_Wpf();
+
+            // Assert
+            Assert.IsNotNull(manager);
+        }
+
+        [TestMethod]
+        public void GenerateBasicHotel_2x3x2() {
+            // TODO: make unit test rather than service test
+            // Arrange
+            //var resolver = new InterfaceResolver();
+            //resolver.Register<IRoomAccess, MockRoomAccess>();
+            //var manager = new OccupancyManager_Wpf(resolver);
+            var manager = new OccupancyManager_Wpf();
+
+            // Act
+            manager.GenerateBasicHotel(2, 3, 2);
+
+            // Assert
+            var rooms = manager.GetAllRooms();
+            Assert.AreEqual(2*3*2, rooms.Count);
+        }
+    }
+
+    public class MockRoomAccess : IRoomAccess {
+        #region Implementation of IDisposable
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose() {}
+
+        #endregion
+
+        #region Implementation of IRoomAccess
+
+        public void GenerateBasicHotel(uint xSize, uint ySize, uint zSize) {}
+
+        public List<string> GetRoomNumbers() {
+            throw new NotImplementedException();
+        }
+
+        public void AssignGuestToRoom(Access.Guest guest, string roomNumber) {
+            throw new NotImplementedException();
+        }
+
+        public void UnassignGuestFromRoom(Access.Guest guest, string roomNumber) {
+            throw new NotImplementedException();
+        }
+
+        public List<Access.Guest> GetGuestList() {
+            throw new NotImplementedException();
+        }
+
+        public List<Access.Room> GetAllEmptyRooms() {
+            throw new NotImplementedException();
+        }
+
+        public List<Access.Room> GetAllAssignedRooms() {
+            throw new NotImplementedException();
+        }
+
+        public List<Access.Room> GetRoomList() {
+            return new List<Access.Room>
+                       {
+                           new Access.Room("1-1-1", new Point3D(1, 1, 1)), 
+                           new Access.Room("1-1-2", new Point3D(1, 1, 2)),
+                           new Access.Room("2-1-1", new Point3D(2, 1, 1))
+                       };
+        }
+
+        #endregion
+    }
+}
---
+++ HotelApp/OccupancyManager/IOccupancyManager_Wpf.cs
@@ -0,0 +1,67 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Windows.Media.Media3D;
+
+#endregion
+
+namespace HotelCorp.HotelApp.Services.Managers {
+    [ServiceContract]
+    public interface IOccupancyManager_Wpf {
+        [OperationContract]
+        List<Room> GenerateBasicHotel(uint roomCountHorizontal, uint roomCountDepth, uint roomCountVerticle);
+
+        [OperationContract]
+        void CheckingGuest(Guest guest, string roomNumber);
+
+        [OperationContract]
+        void CheckoutGuest(Guest guest, string roomNumber);
+
+        [OperationContract]
+        void FindGuest(string firstName, string lastName);
+
+        [OperationContract]
+        List<Room> GetAllRooms();
+    }
+
+    [DataContract]
+    public class Room {
+        [DataMember] public Guest Guest;
+        [DataMember] public Point3D Location;
+        [DataMember] public string RoomNumber;
+
+        public Room(string roomNumber, Point3D location) {
+            RoomNumber = roomNumber;
+            Location = location;
+        }
+
+        public static implicit operator Room(Access.Room fromRoom) {
+            if (fromRoom == null) {
+                return null;
+            }
+            var toRoom = new Room(fromRoom.RoomNumber, fromRoom.Location) {Guest = fromRoom.Guest};
+            return toRoom;
+        }
+    }
+
+    [DataContract]
+    public class Guest {
+        [DataMember] public string FirstName;
+        [DataMember] public string LastName;
+
+        public Guest(string fname, string lname) {
+            FirstName = fname;
+            LastName = lname;
+        }
+
+        public static implicit operator Guest(Access.Guest fromGuest) {
+            if (fromGuest == null) {
+                return null;
+            }
+            var toGuest = new Guest(fromGuest.FirstName, fromGuest.LastName);
+            return toGuest;
+        }
+    }
+}
